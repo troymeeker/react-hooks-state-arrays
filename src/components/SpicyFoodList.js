3b7fda52957d6089fe5e1310de33d0e230b5c72a
troymeeker <troymeeker1@gmail.com>
2021-09-27 12:54:20 -0700

    complete

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -2,19 +2,62 @@ import React, { useState } from "react";
 import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
+  
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState('All');
 
-  function handleAddFood() {
+   function handleAddFood() {
     const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
     console.log(newFood);
+  } 
+  function handleClick(id){
+    const newFoodArray = foods.map((food)=>{
+      if(food.id === id){
+        return{
+          ...food,
+          heatLevel: food.heatLevel +1,
+         }
+      }else{
+        return food;
+      }
+    });
+   setFoods(newFoodArray)
+  }
+  function handleFilter(e){
+    setFilterBy(e.target.value)
   }
 
-  return (
+  const foodsToDisplay = foods.filter((food)=>{
+    if(filterBy==='All'){
+      return true;
+    }else{
+      return food.cuisine === filterBy;
+    }
+  });
+
+ const foodList= foodsToDisplay.map((food)=>(
+ <li key={food.id} onClick={()=>handleClick(food.id)}>
+   {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+ </li>));
+  
+  return(
     <div>
+    <select name="filter" onChange={handleFilter}>
+          <option value="All">All</option>
+          <option value="American">American</option>
+          <option value="Sichuan">Sichuan</option>
+          <option value="Thai">Thai</option>
+          <option value="Mexican">Mexican</option>
+        </select>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
-    </div>
+              <ul>{foodList}</ul>
+      </div>
+    
   );
 }
+  
+
 
 export default SpicyFoodList;
